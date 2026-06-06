9549e221b534e51eafb473fc09a5ba844db914ce
dy0917 <huangkingsley@gmail.com>
2026-06-06 13:51:12 +1200

    add post table

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./dbConnect");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
 
 // parse requests of content-type - application/json
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my MySQL application." });
 });
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
